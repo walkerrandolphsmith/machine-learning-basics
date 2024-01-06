29cacea9c413332ca90249c6514ec116cd72f098
Walker Smith <walkersmith@microsoft.com>
2024-01-05 21:13:49 -0500

    initial commit

---
+++ environment_info.py
@@ -0,0 +1,23 @@
+import sys
+print("Python version: {}".format(sys.version))
+import pandas as pd
+print("pandas version: {}".format(pd.__version__))
+import matplotlib
+print("matplotlib version: {}".format(matplotlib.__version__))
+import numpy as np
+print("NumPy version: {}".format(np.__version__))
+import scipy as sp
+print("SciPy version: {}".format(sp.__version__))
+import IPython
+print("IPython version: {}".format(IPython.__version__))
+import sklearn
+print("scikit-learn version: {}".format(sklearn.__version__))
+
+# Example output
+# Python version: 3.11.4 | packaged by Anaconda, Inc. | (main, Jul  5 2023, 13:38:37) [MSC v.1916 64 bit (AMD64)]
+# pandas version: 1.5.3
+# matplotlib version: 3.7.1
+# NumPy version: 1.24.3
+# SciPy version: 1.10.1
+# IPython version: 8.12.0
+# scikit-learn version: 1.3.0
